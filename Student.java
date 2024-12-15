3f57ea04aa797ba2a8e2aad92ddc94241f8ea12c
Khublal-Kumar <kkhublal030@gmail.com>
2024-12-15 12:24:00 +0530

    commit-6 working fine

--- Student.java
+++ Student.java
@@ -1,13 +1,11 @@
 public class Student {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("hello1 git");
+
+        System.out.println("hello git");
         System.out.println("Line2");
         System.out.println("Line3");
         System.out.println("line4");
         System.out.println("this code for remove");
-=======
-        System.out.println("hello git");
->>>>>>> parent of dee72af (commit-2)
+
     }
 }
